3788b15c1ab1bbe22c2b0b7b25d09daec21e1f97
Danny Krolikowski <krolikowski@utexas.edu>
2023-10-16 13:31:37 -0700

    Create trace_echelle.py
    Created file to hold functions for fitting the trace.

---
+++ trace_echelle.py
@@ -0,0 +1,16 @@
+""" Functions to find the echelle orders on the echellogram and fit their traces.
+
+Created by DMK on 10/16/2023
+Last updated by DMK on 10/16/2023
+"""
+
+##### Imports
+import numpy as np
+import matplotlib.pyplot as plt
+
+from scipy.signal import find_peaks
+
+##### Functions
+
+
+##### Main wrapper script to find and fit the trace
