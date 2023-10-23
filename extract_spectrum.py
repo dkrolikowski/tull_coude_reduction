78dbe7c59eeb27035a78eed8299978364e293edb
Danny Krolikowski <krolikowski@utexas.edu>
2023-10-22 20:48:56 -0700

    Create extract_spectrum.py
    Created the file to hold functions for extracting spectra.

---
+++ extract_spectrum.py
@@ -0,0 +1,12 @@
+""" Functions for extracting the echelle spectra.
+
+Created by DMK on 10/22/2023
+"""
+
+##### Imports
+
+import numpy as np
+
+from scipy.optimize import curve_fit
+
+##### Functions
