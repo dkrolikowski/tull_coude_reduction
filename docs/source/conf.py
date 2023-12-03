e8f27122a69b57fc4010839ee44be412400f2100
Danny Krolikowski <krolikowski@utexas.edu>
2023-12-03 12:40:25 -0700

    Changed doc theme
    Changed the theme for the documentation to sphinx-rtd-theme.
    
    Also added a custom CSS option to have table entries wrap.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath('../../modules/'))
 
@@ -18,7 +19,7 @@ author = 'Daniel Krolikowski'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.napoleon', 'autoapi.extension']
+extensions = ['sphinx.ext.napoleon', 'autoapi.extension', 'sphinx_rtd_theme']
 
 autoapi_dirs = ['../../modules']
 autoapi_type = "python"
@@ -31,6 +32,16 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinxdoc'
+# html_theme = 'sphinxdoc'
+# html_static_path = ['_static']
+# #html_css_files = ['custom.css']
+# html_theme_options = { 'sidebarwidth': '28em' }
+
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_theme_options = { 'sidebarwidth': '28em' }
+html_css_files = ['custom.css']
+
+html_theme_options = {
+    'collapse_navigation': False,
+}
+
